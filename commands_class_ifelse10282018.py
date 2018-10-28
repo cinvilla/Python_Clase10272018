4b821491d06808a54dcf5514664c230d0331effb
cinvilla <cylivralaj27@gmail.com>
2018-10-28 16:52:04 -0600

    Ifelse - elif scripts executed in class

---
+++ commands_class_ifelse10282018.py
@@ -0,0 +1,21 @@
+# ejemplo if else - evaluar expresiones booleanas
+
+a = 10
+b = 5
+if a < b:
+    print('Sí, a < b')
+
+else:
+    print('No, a < b')
+
+# ejemplo if else - evaluar expresiones booleanas
+
+a = 10
+b = 5
+if a < b:
+    print('Sí, a < b')
+elif a == b:
+    assert a != b, 'Error, no debería ser igual'
+else:
+    # TODO
+
