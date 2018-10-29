73d70c44564d4596096204b7c937489ede208318
cinvilla <cylivralaj27@gmail.com>
2018-10-29 17:08:23 -0600

    Rename print_forloops_tests10292018.py to Trabajo en Casa/print_forloops_tests10292018.py
    Moving file to another sub folder

---
+++ Trabajo en Casa/print_forloops_tests10292018.py
@@ -0,0 +1,2 @@
+for posición,valor in enumerate(('Jose', 'Deybid', 'Alexander','Andres')):
+    print('Hola {} esperamos tenga un hermoso dia! Su posición en la lista es {}' .format(posición,valor))
--- print_forloops_tests10292018.py
+++
@@ -1,2 +0,0 @@
-for posición,valor in enumerate(('Jose', 'Deybid', 'Alexander','Andres')):
-    print('Hola {} esperamos tenga un hermoso dia! Su posición en la lista es {}' .format(posición,valor))
