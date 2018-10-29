3be46b978dee57d7dbd40c8338b157d66392fd65
cvillalobos8 <cylivralaj27@gmail.com>
2018-10-29 17:22:19 -0600

    # Print test to understand enumerate and format

--- Trabajo en Casa/print_forloops_tests10292018.py
+++ Trabajo en Casa/print_forloops_tests10292018.py
@@ -1,2 +1,4 @@
 for posición,valor in enumerate(('Jose', 'Deybid', 'Alexander','Andres')):
     print('Hola {} esperamos tenga un hermoso dia! Su posición en la lista es {}' .format(posición,valor))
+
+# Print test to understand enumerate and format
