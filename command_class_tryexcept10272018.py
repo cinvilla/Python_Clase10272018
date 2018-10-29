d9cd915000e0a52d08bd57d873622021b3cbe678
cinvilla <cylivralaj27@gmail.com>
2018-10-29 07:48:55 -0600

    try except scripts executed in class

--- command_class_tryexcept10272018.py
+++ command_class_tryexcept10272018.py
@@ -1,1 +1,16 @@
 #try except
+a = 10
+b = 5
+
+try:
+    c = a / b
+except ZeroDivisionError
+    print('Tengo un error en la division')
+    c = a
+else:
+    print('Dentro de else')
+finally
+    print('Dentro de finally')
+    pass
+
+
