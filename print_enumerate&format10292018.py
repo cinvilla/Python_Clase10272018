83060f2d7265a4d053c39124c2af0d4aa568be41
cvillalobos8 <cylivralaj27@gmail.com>
2018-10-29 16:59:38 -0600

    Práctica para enterder for, enumerate y .format

--- print_enumerate&format10292018.py
+++ print_enumerate&format10292018.py
@@ -5,20 +5,20 @@ print(enumerate(mi_tupla))
 nueva_tupla = tuple(enumerate(mi_tupla))
 print(nueva_tupla)
 print("#######")
-for index, possition in nueva_tupla:
-    print('Para el valor {} su índice de posición es {}'.format(possition,index))
+for index, value in nueva_tupla:
+    print('Para el valor {} su índice de posición es {}'.format(value,index))
 
 ########################################################################################
 print()
 print("Esta es la prueba dos")
 # Como lo hizo el profe es mucho más fácil porque así simplemente se usa la lista misma sin tener que nombrarla antes
 # y simplemente usando la función enumerate
-for index, possition in enumerate(['Ana','Deybid','Kevin']):
-    print('Para el valor {} el índice de posición es {}'.format(possition,index))
+for index, value in enumerate(['Ana','Deybid','Kevin']):
+    print('Para el valor {} el índice de posición es {}'.format(value,index))
 
 ########################################################################################
 print()
 print("Esta es la prueba tres")
 # Paso a paso para entender el output del format y qué hace
 for posición,valor in enumerate(('Jose', 'Deybid', 'Alexander','Andres')):
-    print('Hola {} esperamos tenga un hermoso dia! Su posición en la lista es {}' .format(posición,valor))
+    print('Hola {} esperamos tenga un hermoso dia! Su posición en la lista es {}' .format(valor,posición))
